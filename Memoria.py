1a85eb3977d18527042ba174dec8699ed4c8b273
AlanFC11 <73494793+AlanFC11@users.noreply.github.com>
2020-10-29 22:28:27 -0600

    Detectar casillas abiertas/ Imágenes para recordar

--- Memoria.py
+++ Memoria.py
@@ -1,12 +1,25 @@
 from random import *
 from turtle import *
 from freegames import path
+import turtle
+from tkinter import PhotoImage
+
+images = ["rayo1.gif", "sol.gif", "abeja.gif", "americano.gif", "arbol.gif", "basket.gif", "balonfutbol.gif", "calabaza.gif",
+          "cerdo.gif", "estrella.gif", "luna.gif", "m.gif", "manzana.gif", "pera.gif", "planta.gif", "sombrero.gif", "pay.gif",
+          "mano.gif", "reloj.gif"]
+smaller = []
+for i in range(len(images)):
+    turtle.register_shape(images[i])
+    smaller.append(PhotoImage(file=images[i]).subsample(8, 8))
+    addshape(("%d" % i), Shape("image", smaller[i]))
+    
 
 car = path('car.gif')
-tiles = list(range(32)) * 2
+tiles = list(range(18)) * 2
 state = {'mark': None}
-hide = [True] * 64
+hide = [True] * 36
 veces = 0
+abiertas = 0
 
 
 def square(x, y):
@@ -24,12 +37,12 @@ def square(x, y):
 
 def index(x, y):
     "Convert (x, y) coordinates to tiles index."
-    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
+    return int((x + 150) // 50 + ((y + 150) // 50) * 6)
 
 
 def xy(count):
     "Convert tiles count to (x, y) coordinates."
-    return (count % 8) * 50 - 200, (count // 8) * 50 - 200
+    return (count % 6) * 50 - 150, (count // 6) * 50 - 150
 
 
 def tap(x, y):
@@ -43,6 +56,13 @@ def tap(x, y):
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
+        global abiertas
+        abiertas += 2
+        print(abiertas)
+        if abiertas == len(tiles):
+            print("Has ganado")
+            goto(240, 100)
+            write("Has ganado", font=('Arial', 15, 'normal'))
     global veces
     veces = veces + 1
 
@@ -54,7 +74,7 @@ def draw():  # dibuja los numeros y la imagen
     shape(car)
     stamp()
 
-    for count in range(64):
+    for count in range(36):
         if hide[count]:
             x, y = xy(count)
             square(x, y)
@@ -65,14 +85,16 @@ def draw():  # dibuja los numeros y la imagen
         x, y = xy(mark)
         up()
         goto(x + 26, y + 9)
-        color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'), align="center")
+        s = Turtle(str(int(tiles[mark])))
+        s.setposition(x+26,y+25)
+        #write(tiles[mark], font=('Arial', 25, 'normal'), align="center")
     goto(240, 130)
-    write(veces, font=('Arial', 30, 'normal'))
+    write(veces, font=('Arial', 25, 'normal'))
 
     update()
     ontimer(draw, 100)
 
+            
 
 shuffle(tiles)
 setup(600, 420, 370, 0)
